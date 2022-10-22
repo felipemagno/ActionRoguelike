9be173e06da7b1d8f01e6c4beeadab75bb0e1b63
FMagno <philippos.magnum@gmail.com>
2022-10-22 09:40:54 -0300

    Materials

---
+++ Source/ActionRoguelike/Private/AR_TargetDummy.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AR_TargetDummy.h"
+
+#include "ActorComponent/AR_AttributeComponent.h"
+
+
+void AAR_TargetDummy::OnHitEvent(AActor* InstigatingActor, UAR_AttributeComponent* OwningAttribute,
+                                 float NewHealthValue, float DeltaValue, float NewHealthPercentage)
+{
+	if (DeltaValue < 0)
+		MeshComp->SetScalarParameterValueOnMaterials("TimeToHit", GetWorld()->TimeSeconds);
+}
+
+// Sets default values
+AAR_TargetDummy::AAR_TargetDummy()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("MeshComponent");
+	RootComponent = MeshComp;
+
+	AttributeComponent = CreateDefaultSubobject<UAR_AttributeComponent>("AttributeComponent");
+	AttributeComponent->OnHealthChanged.AddDynamic(this, &AAR_TargetDummy::OnHitEvent);
+}
+
+// Called when the game starts or when spawned
+void AAR_TargetDummy::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+// Called every frame
+void AAR_TargetDummy::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/ActionRoguelike/Public/AR_TargetDummy.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AR_TargetDummy.generated.h"
+
+class UAR_AttributeComponent;
+
+UCLASS()
+class ACTIONROGUELIKE_API AAR_TargetDummy : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AAR_TargetDummy();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UStaticMeshComponent* MeshComp;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UAR_AttributeComponent* AttributeComponent;
+
+	UFUNCTION()
+	void OnHitEvent(AActor* InstigatingActor, UAR_AttributeComponent* OwningAttribute, float NewHealthValue, float DeltaValue, float NewHealthPercentage);
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
