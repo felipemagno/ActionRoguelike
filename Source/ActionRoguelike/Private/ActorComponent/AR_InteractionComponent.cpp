bae5fbe5b6dfdb7644f032c218a45f7b09d7178f
FMagno <philippos.magnum@gmail.com>
2022-12-26 08:01:55 -0300

    Interaction Widget Prompt

--- Source/ActionRoguelike/Private/ActorComponent/AR_InteractionComponent.cpp
+++ Source/ActionRoguelike/Private/ActorComponent/AR_InteractionComponent.cpp
@@ -3,14 +3,17 @@
 
 #include "ActorComponent/AR_InteractionComponent.h"
 
+#include "Blueprint/UserWidget.h"
 #include "Interfaces/AR_IGameplayInterface.h"
+#include "UserInterface/AR_WorldUserWidget.h"
+#
 
 static TAutoConsoleVariable<bool> CVarDebugDrawInteraction(
 	TEXT("ar_Debug.DrawInteraction"), false,TEXT("Enable drawing debug of the interaction action."),
 	ECVF_Cheat);
 
 // Sets default values for this component's properties
-UAR_InteractionComponent::UAR_InteractionComponent()
+UAR_InteractionComponent::UAR_InteractionComponent(): TraceDistance(500), TraceRadius(30.0f),TraceCollisionChannel(ECC_WorldDynamic)
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -28,22 +31,21 @@ void UAR_InteractionComponent::BeginPlay()
 	// ...
 }
 
-
 // Called every frame
 void UAR_InteractionComponent::TickComponent(float DeltaTime, ELevelTick TickType,
                                              FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	FindBestInteractable();
 }
 
-void UAR_InteractionComponent::PrimaryInteract()
+void UAR_InteractionComponent::FindBestInteractable()
 {
 	bool bIsDebugging = CVarDebugDrawInteraction.GetValueOnGameThread();
 
 	FCollisionObjectQueryParams ObjectQueryParams;
-	ObjectQueryParams.AddObjectTypesToQuery(ECC_WorldDynamic);
+	ObjectQueryParams.AddObjectTypesToQuery(TraceCollisionChannel);
 
 	FVector EndLocation;
 
@@ -52,7 +54,7 @@ void UAR_InteractionComponent::PrimaryInteract()
 	AActor* Owner = GetOwner();
 	Owner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
 
-	EndLocation = EyeLocation + EyeRotation.Vector() * 1000;
+	EndLocation = EyeLocation + EyeRotation.Vector() * TraceDistance;
 
 	// Simple single object Hit trace
 	{
@@ -76,14 +78,14 @@ void UAR_InteractionComponent::PrimaryInteract()
 	// Complex multi object with shape trace
 	{
 		TArray<FHitResult> HitResults;
-		float traceRadius = 30.0f;
 		FCollisionShape CollisionShape;
-		CollisionShape.SetSphere(traceRadius);
+		CollisionShape.SetSphere(TraceRadius);
 
 		bool bBlockingHit = GetWorld()->SweepMultiByObjectType(HitResults, EyeLocation, EndLocation, FQuat::Identity,
 		                                                       ObjectQueryParams,
 		                                                       CollisionShape);
-
+		// try to find best fitting actor to interact
+		FocusedActor = nullptr;
 		for (FHitResult hit : HitResults)
 		{
 			AActor* HitActor = hit.GetActor();
@@ -93,27 +95,74 @@ void UAR_InteractionComponent::PrimaryInteract()
 				if (bIsDebugging)
 				{
 					DebugSphereColor = FColor::Yellow;
-					DrawDebugSphere(GetWorld(), hit.ImpactPoint, traceRadius, 12, DebugSphereColor, false, 3, 0, 2);
+					DrawDebugSphere(GetWorld(), hit.ImpactPoint, TraceRadius, 12, DebugSphereColor, false, 3, 0,
+					                2);
 				}
 				if (HitActor->Implements<UAR_IGameplayInterface>())
 				{
-					APawn* OwnerPawn = Cast<APawn>(Owner);
-					IAR_IGameplayInterface::Execute_Interact(HitActor, OwnerPawn);
+					// Only accepts if there is a clear line of sight to the interactable actor
+					FHitResult VisibilityHit;
+					GetWorld()->LineTraceSingleByChannel(VisibilityHit, EyeLocation, hit.ImpactPoint, ECC_Visibility);
+					if (VisibilityHit.GetActor()) continue;
+
+					FocusedActor = HitActor;
 
 					if (bIsDebugging)
 					{
 						DebugSphereColor = FColor::Green;
-						DrawDebugSphere(GetWorld(), hit.ImpactPoint, traceRadius, 12, DebugSphereColor, false, 3, 0, 2);
+						DrawDebugSphere(GetWorld(), hit.ImpactPoint, TraceRadius, 12, DebugSphereColor, false, 3,
+						                0, 2);
 					}
 					break;
 				}
 			}
 		}
 
+		// Widget feedback for having or not a FocusedActor
+		if (FocusedActor)
+		{
+			if (!InteractWidgetInstance && ensure(InteractWidgetClass))
+			{
+				InteractWidgetInstance = CreateWidget<UAR_WorldUserWidget>(GetWorld(), InteractWidgetClass);
+			}
+
+			if (InteractWidgetInstance)
+			{
+				InteractWidgetInstance->AttachedActor = FocusedActor;
+
+				if (!InteractWidgetInstance->IsInViewport())
+				{
+					InteractWidgetInstance->AddToViewport();
+				}
+			}
+		}
+		else
+		{
+			if (InteractWidgetInstance && InteractWidgetInstance->IsInViewport())
+			{
+				InteractWidgetInstance->RemoveFromParent();
+			}
+		}
+
+
+		// Debug Line for Interaction Attempt
 		if (bIsDebugging)
 		{
 			FColor LineColor = bBlockingHit ? FColor::Green : FColor::Red;
 			DrawDebugLine(GetWorld(), EyeLocation, EndLocation, LineColor, false, 3, 0, 2);
 		}
 	}
+}
+
+
+void UAR_InteractionComponent::PrimaryInteract()
+{
+	if (FocusedActor == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, "No focus actor to interact.");
+		return;
+	}
+
+	APawn* OwnerPawn = Cast<APawn>(GetOwner());
+	IAR_IGameplayInterface::Execute_Interact(FocusedActor, OwnerPawn);
 }
--- Source/ActionRoguelike/Public/ActorComponent/AR_InteractionComponent.h
+++ Source/ActionRoguelike/Public/ActorComponent/AR_InteractionComponent.h
@@ -6,6 +6,7 @@
 #include "Components/ActorComponent.h"
 #include "AR_InteractionComponent.generated.h"
 
+class UAR_WorldUserWidget;
 
 UCLASS(ClassGroup=(ActionRoguelike), meta=(BlueprintSpawnableComponent))
 class ACTIONROGUELIKE_API UAR_InteractionComponent : public UActorComponent
@@ -19,10 +20,32 @@ public:
 	UAR_InteractionComponent();
 
 protected:
+	UPROPERTY(BlueprintReadOnly, Category="Interaction")
+	AActor* FocusedActor;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Interaction")
+	TSubclassOf<UAR_WorldUserWidget> InteractWidgetClass;
+
+	UPROPERTY(BlueprintReadOnly, Category="Interaction")
+	UAR_WorldUserWidget* InteractWidgetInstance;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Interaction")
+	float TraceDistance;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Interaction")
+	float TraceRadius;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Interaction")
+	TEnumAsByte<ECollisionChannel> TraceCollisionChannel;
+	
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	void FindBestInteractable();
+
+
 public:
+	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
